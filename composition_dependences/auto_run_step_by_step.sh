4a7e68631ac2ec61a192b4b62fe2e07f660f73a6
howmarketing <ligados.adwords@gmail.com>
2021-11-03 18:42:58 -0300

    alterando os nomes dos containers de acordo com o nome da nova pasta na nova versão

--- composition_dependences/auto_run_step_by_step.sh
+++ composition_dependences/auto_run_step_by_step.sh
@@ -5,12 +5,12 @@ sudo docker-compose up -d
 
 # exec bash script file updating mysql password authenticat to mysql_native_password for global connection as root user
 
-sudo docker exec -it app_server_mysqlsrv_1 bash /var/lib/mysql/mysql_set_native_password.sh
+sudo docker exec -it composition_dependences_mysqlsrv_1 bash /var/lib/mysql/mysql_set_native_password.sh
 
 # start mysql service to be able to connect through mysql cli library dependences
 
-sudo docker exec -it app_server_server_app_1 bash service mysql start
+sudo docker exec -it composition_dependences_server_app_1 bash service mysql start
 
 # exec git shell script seting up git init and remote to app base repository
 
-sudo docker exec -it app_server_server_app_1 bash /usr/local/apache2/htdocs/app/git_presets.sh 
+sudo docker exec -it composition_dependences_server_app_1 bash /usr/local/apache2/htdocs/app/git_presets.sh 
