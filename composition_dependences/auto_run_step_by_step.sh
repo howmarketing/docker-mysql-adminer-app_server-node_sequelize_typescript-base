007fadcc22598febd62c27abb842b6f7410d5b61
Gabriel Ariza Gomes de Castro <gabriel@arizapp.com.br>
2021-11-03 19:31:40 -0300

    fixing errors and testing

--- composition_dependences/auto_run_step_by_step.sh
+++ composition_dependences/auto_run_step_by_step.sh
@@ -4,20 +4,21 @@ echo "Start docker pose upping images already setting up for mysql and server us
 
 sudo docker-compose up -d
 
-echo "in 20 seconds - exec bash script file updating mysql password authenticat to mysql_native_password for global connection as root user"
+echo "See docker containers running in 10s"
+sleep 10
 
-sleep 20
+docker ps
+
+echo "In 10 seconds - exec bash script file updating mysql password authenticat to mysql_native_password for global connection as root user"
+
+sleep 10
 
 sudo docker exec -it composition_dependences_mysqlsrv_1 bash /var/lib/mysql/mysql_set_native_password.sh
 
-echo "in 20 seconds start mysql service to be able to connect through mysql cli library dependences"
-
-sleep 20
+echo "Start mysql service to be able to connect through mysql cli library dependences"
 
 sudo docker exec -it composition_dependences_server_app_1 bash service mysql start
 
-echo "in 20 seconds  exec git shell script seting up git init and remote to app base repository"
-
-sleep 20
+echo "Exec git shell script seting up git init and remote to app base repository"
 
 sudo docker exec -it composition_dependences_server_app_1 bash /usr/local/apache2/htdocs/app/git_presets.sh 
