5bf89bf1c2d6db563d55c56effb8d0a2e1d2c603
Gabriel Ariza Gomes de Castro <gabriel@arizapp.com.br>
2021-11-03 19:27:12 -0300

    go to dir and exec

--- app/git_presets.sh
+++ app/git_presets.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-git init .
-
-git pull https://github.com/howmarketing/sequelize-typescript.git master
-
-ls -lh
+cd /usr/local/apache2/htdocs/app && git init . && git pull https://github.com/howmarketing/sequelize-typescript.git master && ls -lh && echo "MY DIR ${PWD}"
