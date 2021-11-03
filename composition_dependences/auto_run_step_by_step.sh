3b7c4917194cdcd778fe645792c61248b97fa886
howmarketing <ligados.adwords@gmail.com>
2021-11-03 18:49:47 -0300

    adding delay time to exec in docker container

--- composition_dependences/auto_run_step_by_step.sh
+++ composition_dependences/auto_run_step_by_step.sh
@@ -3,14 +3,20 @@
 # start docker pose upping images already setting up for mysql and server using apache as virtualization network and nodejs
 sudo docker-compose up -d
 
-# exec bash script file updating mysql password authenticat to mysql_native_password for global connection as root user
+echo "in 20 seconds - exec bash script file updating mysql password authenticat to mysql_native_password for global connection as root user"
+
+sleep 20
 
 sudo docker exec -it composition_dependences_mysqlsrv_1 bash /var/lib/mysql/mysql_set_native_password.sh
 
-# start mysql service to be able to connect through mysql cli library dependences
+echo "in 20 seconds start mysql service to be able to connect through mysql cli library dependences"
+
+sleep 20
 
 sudo docker exec -it composition_dependences_server_app_1 bash service mysql start
 
-# exec git shell script seting up git init and remote to app base repository
+echo "in 20 seconds  exec git shell script seting up git init and remote to app base repository"
+
+sleep 20
 
 sudo docker exec -it composition_dependences_server_app_1 bash /usr/local/apache2/htdocs/app/git_presets.sh 
